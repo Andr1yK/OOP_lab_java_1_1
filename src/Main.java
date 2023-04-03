c6678b3f8da59974ec73b0ff052c1a9ab2b3f660
Andriy Kobetiak <andriykobetiak@gmail.com>
2023-04-04 01:00:36 +0300

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double x, y, A, B;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("x = ");
+        x = scanner.nextDouble();
+
+        A = (2 + x) / Math.pow(x, 2) + 1;
+
+        // спосіб 1: розгалуження в скороченій формі
+        if (x < 0) {
+            B = Math.pow(x, 3) - 2 * Math.pow(x, 4);
+        } else if (0 <= x && x <= 2) {
+            B = Math.pow(Math.abs(x) + Math.exp(x), 3);
+        } else {
+            B = 4 * Math.cos(Math.pow(x, 2) - 2);
+        }
+
+        y = A + B;
+
+        System.out.println();
+        System.out.println("1) y = " + y);
+
+        // спосіб 2: розгалуження в повній формі
+        if (x < 0) {
+            B = Math.pow(x, 3) - 2 * Math.pow(x, 4);
+        } else if (x > 2) {
+            B = 4 * Math.cos(Math.pow(x, 2) - 2);
+        } else {
+            B = Math.pow(Math.abs(x) + Math.exp(x), 3);
+        }
+
+        y = A + B;
+
+        System.out.println("2) y = " + y);
+
+        scanner.close();
     }
 }
